7d59e0e6e1b691600ecb473ebb9e3da7a835e8bc
이준혁 <merong245@gmail.com>
2023-09-19 21:24:49 +0900

    init : spring jpa, spring web, spring rest docs, h2 console, lombok 의존성 설정

---
+++ src/main/java/com/example/apidocsstudy/ApiDocsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.apidocsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDocsStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apidocsstudy/ApiDocsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apidocsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDocsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
